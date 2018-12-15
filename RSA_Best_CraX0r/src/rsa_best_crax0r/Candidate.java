df9219b1346a8651b51c189c8c3915834fa908e3
RiPs7 <perimara@hotmail.com>
2018-12-15 21:06:15 +0000

    Add files via upload

--- RSA_Best_CraX0r/src/rsa_best_crax0r/Candidate.java
+++ RSA_Best_CraX0r/src/rsa_best_crax0r/Candidate.java
@@ -4,6 +4,10 @@ public class Candidate {
     
     public int p, q, carryOver;
     
-    
+    public Candidate(int p, int q, int carryOver){
+        this.p = p;
+        this.q = q;
+        this.carryOver = 0;
+    }
     
 }
--- RSA_Best_CraX0r/src/rsa_best_crax0r/CraX0r.java
+++ RSA_Best_CraX0r/src/rsa_best_crax0r/CraX0r.java
@@ -19,7 +19,21 @@ public class CraX0r {
         
         Stack<Candidate> stack = new Stack();
         
-        //while 
+        int digits = 1;
+        int lastPart = n % ((int)Math.pow(10, digits));
+        if (lastPart == 1) {
+            stack.push(new Candidate(1, 1, 0));
+            stack.push(new Candidate(9, 9, 8));
+        } else if (lastPart % 3 == 0) {
+            stack.push(new Candidate(1, 3, 0));
+            stack.push(new Candidate(7, 9, 6));
+        } else if (lastPart % 7 == 0) {
+            stack.push(new Candidate(1, 7, 0));
+            stack.push(new Candidate(3, 9, 2));
+        } else if (lastPart % 9 == 0) {
+            stack.push(new Candidate(1, 9, 0));
+            stack.push(new Candidate(3, 3, 0));
+        }
         
         return null;
     }
